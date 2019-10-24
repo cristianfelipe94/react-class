cdfd180225c06f025198c1fa96a3fd6d2db7bf13
unknown <cristianpipe0494@gmail.com>
2019-10-23 19:01:36 -0600

    Have added into pokedex functionality to search.

--- pokedex-api/src/App.js
+++ pokedex-api/src/App.js
@@ -1,43 +1,106 @@
-import React, {Component} from 'react';
-import PokeCard from './PokeCard.js';
-import PokeData from './service.js';
-import './app.css';
+import React, { Component } from "react";
+import PokeCard from "./PokeCard.js";
+import PokeData from "./service.js";
+import "./app.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cards: []
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			cards: {
+				render: [],
+				stock: []
+			},
+			searchInput: ""
+		};
+		this.searchFor = this.searchFor.bind(this);
+	}
 
-  async componentDidMount() {
-    try {
-      let dataPokeProcessed = await PokeData();
-      const pokeCard = dataPokeProcessed.map((card, id) => {
-        return(
-          <PokeCard pokeName={card.name} pokeImage={card.imageUrlHiRes} key={id}/>
-        );
-      });
-      this.setState({
-        cards: pokeCard
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	async componentDidMount() {
+		try {
+			let dataPokeProcessed = await PokeData();
+			this.setState({
+				cards: {
+					...this.state.cards,
+					stock: dataPokeProcessed,
+					render: dataPokeProcessed
+				}
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  render() {
-    return(
-      <div>
-        <h1>Pokedex</h1>
-        <div className="pokedex">
-          <div className="pokedex--layout"></div>
-          {this.state.cards}
-        </div>
-      </div>
-    )
-  }
+	searchFor(event) {
+		const statename = event.target.id;
+		this.setState(
+			{
+				[statename]: event.target.value,
+				cards: {
+					...this.state.cards,
+					render: this.state.cards.stock
+				}
+			},
+			() => {
+				const searchedArray = [];
+				const nameArray = this.state.cards.render.map(card => {
+					return card.name;
+				});
+				nameArray.forEach(cardName => {
+					if (cardName.indexOf(this.state.searchInput) > -1) {
+						const matchCard = nameArray.indexOf(cardName);
+						searchedArray.push(this.state.cards.render[matchCard]);
+					}
+				});
+				this.setState({
+					cards: {
+						...this.state.cards,
+						render: searchedArray
+					}
+				});
+			}
+		);
+	}
+
+	render() {
+		const pokeCard = this.state.cards.render.map((card, id) => {
+			const pokeHp = card.hp ? card.hp : "None";
+			const poketype = card.types ? card.types[0] : "None";
+			const specialCard = card.text ? card.text[0] : "None";
+			return (
+				<PokeCard
+					pokeName={card.name}
+					pokeImage={card.imageUrlHiRes}
+					hp={pokeHp}
+					subtype={card.supertype}
+					type={poketype}
+					text={specialCard}
+					key={id}
+				/>
+			);
+		});
+		return (
+			<div className="pokeapp">
+				<div className="pokeapp__head">
+					<h1>Pokedex</h1>
+					<label htmlFor="pokeSearch">
+						Search for:
+						<input
+							id="searchInput"
+							className="search--bar"
+							placeholder="Pokemon's name"
+							onChange={this.searchFor}
+						></input>
+					</label>
+				</div>
+				<div className="pokedex">
+					<div className="pokedex--layout"></div>
+          <div className="clear--pokedex"></div>
+          {pokeCard}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- pokedex-api/src/PokeCard.js
+++ pokedex-api/src/PokeCard.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './app.css';
 
-export default ({pokeName, pokeImage}) => {
+export default ({pokeName, pokeImage, hp, subtype, type, text}) => {
   return(
     <div className={'pokedex__info'}>
       <img src={pokeImage} className='card'/>
       <div className="info__block">
         <h2>{pokeName}</h2>
+        {hp !== "None" ? <p>HP:{hp}</p> : ""}
+        <p>This is a {subtype} card.</p>
+        {type !== "None" ? <p>This is a {subtype} type {type}</p> : ""}
+        {text !== "None" ? <p>{text}</p> : ""}
       </div>
     </div>
   )
